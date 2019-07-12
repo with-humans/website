467afec95927ed0356a2cbc6bfcae894472ab7ae
Raimo Radczewski <raimoradczewski@googlemail.com>
2019-07-12 13:59:48 +0200

    Replace eleventy with jekyll

--- .eleventy.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("favicons/.");
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("js");
-  };
